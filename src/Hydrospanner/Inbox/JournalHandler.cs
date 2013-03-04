fc0d22af99f849166fc43eef1cc1190fbc08453d
Oliver, Jonathan <joliver@localhost>
2013-03-04 14:07:45 -0700

    Added TODO.

--- src/Hydrospanner/Inbox/JournalHandler.cs
+++ src/Hydrospanner/Inbox/JournalHandler.cs
@@ -1,13 +1,41 @@
 ﻿namespace Hydrospanner.Inbox
 {
+	using System.Collections.Generic;
+	using System.Configuration;
 	using Disruptor;
 
 	public class JournalHandler : IEventHandler<WireMessage>
 	{
 		public void OnNext(WireMessage data, long sequence, bool endOfBatch)
 		{
+			// TODO: where does de-duplication happen?
+
+			data.IncomingSequence = ++this.storedSequence;
 			// TODO: callback into application code to determine the stream of a given message
-			// at end of batch, write incoming message bytes to disk
+
+			this.buffer.Add(data);
+			if (endOfBatch)
+				this.JournalMessages();
 		}
+		private void JournalMessages()
+		{
+			// TODO: append sequence number to each journaled message
+			using (var connection = this.settings.OpenConnection())
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = string.Empty;
+				command.ExecuteNonQuery(); // TODO: circuit breaker pattern
+			}
+		}
+
+		public JournalHandler(string connectionName)
+		{
+			// TODO: get max sequence number
+			this.settings = ConfigurationManager.ConnectionStrings[connectionName];
+		}
+
+		private readonly List<WireMessage> buffer = new List<WireMessage>();
+		private readonly ConnectionStringSettings settings;
+		private long storedSequence;
 	}
 }
