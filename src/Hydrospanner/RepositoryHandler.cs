a43314a2f2d2bc2fa513f50fb33a112fbcc78073
Oliver, Jonathan <joliver@localhost>
2013-03-06 14:08:26 -0700

    Implemented repository.

--- src/Hydrospanner/RepositoryHandler.cs
+++ src/Hydrospanner/RepositoryHandler.cs
@@ -1,5 +1,9 @@
 ﻿namespace Hydrospanner
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Text;
 	using Disruptor;
 
 	public class RepositoryHandler : IEventHandler<WireMessage>
@@ -8,6 +12,88 @@
 		{
 			if (data.DuplicateMessage)
 				return;
+
+			if (!this.cache.ContainsKey(data.StreamId))
+				this.missingStreams.Add(data.StreamId);
+
+			this.buffer.Add(data);
+			if (!endOfBatch)
+				return;
+
+			using (var connection = this.settings.OpenConnection())
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "SELECT stream_id, payload, headers FROM messages WHERE sequence <= (SELECT sequence FROM checkpoints) AND stream_id IN ({1});".FormatWith(this.checkpoint, this.GetUncachedStreamIdentifiers());
+				using (var reader = command.ExecuteReader())
+				{
+					if (reader == null)
+						return;
+
+					while (reader.Read())
+					{
+						var claimed = this.ring.Next();
+						var message = this.ring[claimed];
+
+						var streamId = reader.GetGuid(0);
+						message.Hydratables = this.cache[streamId];
+						message.SerializedBody = (reader[1] as byte[]);
+						message.SerializedHeaders = (reader[2] as byte[]);
+
+						ring.Publish(claimed);
+					}
+				}
+			}
+
+			foreach (var item in this.buffer)
+			{
+				var claimed = this.ring.Next();
+				var message = this.ring[claimed];
+				message.Body = item.Body;
+				message.Headers = item.Headers;
+				message.MessageSequence = item.MessageSequence;
+				message.Hydratables = this.cache[item.StreamId];
+				message.Replay = item.MessageSequence > this.checkpoint;
+
+				ring.Publish(claimed);
+			}
+
+			this.buffer.Clear();
 		}
+		private string GetUncachedStreamIdentifiers()
+		{
+			if (this.missingStreams.Count == 0)
+				return string.Empty;
+
+			var builder = new StringBuilder();
+
+			foreach (var stream in this.missingStreams)
+			{
+				builder.AppendFormat("'{0}',", stream);
+				this.cache[stream] = this.factory(stream);
+			}
+
+			var identifiers = builder.ToString();
+			return identifiers.Substring(0, identifiers.Length - 1);
+		}
+
+		public RepositoryHandler(
+			RingBuffer<TransformationMessage> ring,
+			string connectionName,
+			long checkpoint,
+			Func<Guid, IHydratable[]> factory)
+		{
+			this.settings = ConfigurationManager.ConnectionStrings[connectionName];
+			this.checkpoint = checkpoint;
+			this.factory = factory;
+			this.ring = ring;
+		}
+
+		private readonly Dictionary<Guid, IHydratable[]> cache = new Dictionary<Guid, IHydratable[]>(); // TODO: MRU
+		private readonly List<WireMessage> buffer = new List<WireMessage>();
+		private readonly HashSet<Guid> missingStreams = new HashSet<Guid>();
+		private readonly RingBuffer<TransformationMessage> ring;
+		private readonly ConnectionStringSettings settings;
+		private readonly long checkpoint;
+		private readonly Func<Guid, IHydratable[]> factory;
 	}
 }
--- src/Hydrospanner/TransformationMessage.cs
+++ src/Hydrospanner/TransformationMessage.cs
@@ -6,7 +6,6 @@
 	public sealed class TransformationMessage
 	{
 		public long MessageSequence { get; set; }
-		public Guid StreamId { get; set; }
 		public bool Replay { get; set; } // have we ever seen and handled this message before?
 
 		public byte[] SerializedBody { get; set; }
@@ -15,12 +14,11 @@
 		public object Body { get; set; }
 		public Dictionary<string, string> Headers { get; set; }
 
-		public IHydratable<object>[] Hydratables { get; set; }
+		public IHydratable[] Hydratables { get; set; }
 
 		public void Clear()
 		{
 			this.MessageSequence = 0;
-			this.StreamId = Guid.Empty;
 			this.Replay = false;
 			this.SerializedBody = null;
 			this.SerializedHeaders = null;
