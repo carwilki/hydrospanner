737bb94db042a6b2306a79f078bf924a03720a31
Oliver, Jonathan <joliver@localhost>
2013-03-08 22:39:46 -0700

    Avoiding deadlocks when forwarding.

--- src/Hydrospanner/ForwardLocalHandler.cs
+++ src/Hydrospanner/ForwardLocalHandler.cs
@@ -1,5 +1,7 @@
 ﻿namespace Hydrospanner
 {
+	using System;
+	using System.Collections.Generic;
 	using Disruptor;
 
 	public class ForwardLocalHandler : IEventHandler<DispatchMessage>
@@ -9,13 +11,24 @@
 			if (data.AcknowledgeDelivery != null)
 				return; // this message has already been handled by the wire ring buffer
 
-			var claimed = this.ring.Next();
-			var item = this.ring[claimed];
-			item.Clear();
-			item.MessageSequence = data.MessageSequence;
-			item.Body = data.Body;
-			item.Headers = data.Headers;
-			this.ring.Publish(claimed);
+			this.buffer.Enqueue(data); // avoid Disruptor ring buffer deadlocks by enqueuing locally and then attempting to forward if a slot is available
+
+			while (this.buffer.Count > 0)
+			{
+				var claimed = this.ring.Next(Timeout); // deadlocks can occur if the DB is down for a long time and the WireMessage ring buffer fills up.
+				if (claimed == 0)
+					return;
+
+				var message = this.buffer.Dequeue();
+				var item = this.ring[claimed];
+				item.Clear();
+
+				item.MessageSequence = message.MessageSequence;
+				item.Body = message.Body;
+				item.Headers = message.Headers;
+
+				this.ring.Publish(claimed);
+			}
 		}
 
 		public ForwardLocalHandler(RingBuffer<WireMessage> ring)
@@ -23,6 +36,8 @@
 			this.ring = ring;
 		}
 
+		private static readonly TimeSpan Timeout = TimeSpan.FromSeconds(5);
+		private readonly Queue<DispatchMessage> buffer = new Queue<DispatchMessage>();
 		private readonly RingBuffer<WireMessage> ring;
 	}
 }
