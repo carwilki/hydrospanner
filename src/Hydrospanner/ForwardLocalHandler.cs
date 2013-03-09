a23727cadced37022be819aaa7d51eb437d26e4b
Oliver, Jonathan <joliver@localhost>
2013-03-08 22:27:24 -0700

    Added ability to forward dispatched messages back to source ring buffer.

--- src/Hydrospanner/ForwardLocalHandler.cs
+++ src/Hydrospanner/ForwardLocalHandler.cs
@@ -6,6 +6,9 @@
 	{
 		public void OnNext(DispatchMessage data, long sequence, bool endOfBatch)
 		{
+			if (data.AcknowledgeDelivery != null)
+				return; // this message has already been handled by the wire ring buffer
+
 			var claimed = this.ring.Next();
 			var item = this.ring[claimed];
 			item.Clear();
