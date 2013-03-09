c75520d37d4dd2dceb445a3e3c8a9c8c450899f6
Oliver, Jonathan <joliver@localhost>
2013-03-08 21:17:51 -0700

    Added generic adapter.

---
+++ src/Hydrospanner/GenericHydratableSelector.cs
@@ -0,0 +1,36 @@
+﻿namespace Hydrospanner
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class GenericHydratableSelector : IHydratableSelector
+	{
+		public void Register<T>(IHydratableSelector<T> selector)
+		{
+			this.selectors.Add(typeof(T), new GenericAdapter<T>(selector));
+		}
+		public IEnumerable<IHydratableKey> Keys(object message, Dictionary<string, string> headers = null)
+		{
+			IHydratableSelector selector;
+			if (this.selectors.TryGetValue(message.GetType(), out selector))
+				return selector.Keys(message, headers);
+
+			return new IHydratableKey[0];
+		}
+
+		private readonly Dictionary<Type, IHydratableSelector> selectors = new Dictionary<Type, IHydratableSelector>();
+
+		private class GenericAdapter<T> : IHydratableSelector
+		{
+			public IEnumerable<IHydratableKey> Keys(object message, Dictionary<string, string> headers = null)
+			{
+				return this.selector.Keys((T)message, headers);
+			}
+			public GenericAdapter(IHydratableSelector<T> selector)
+			{
+				this.selector = selector;
+			}
+			private readonly IHydratableSelector<T> selector;
+		}
+	}
+}
