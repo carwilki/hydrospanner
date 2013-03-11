e111a637ca97d8c9ccc1ac3e863d2b9348a7784f
Oliver, Jonathan <joliver@localhost>
2013-03-11 14:30:13 -0600

    Added system snapshot capabilities.

--- src/Hydrospanner/SystemSnapshotHandler.cs
+++ src/Hydrospanner/SystemSnapshotHandler.cs
@@ -9,8 +9,32 @@
 			if (data.IsolatedSnapshot)
 				return;
 
-			// these will only ever come in a batch; just stream until the last item is reached and then close the stream
-			// at the first item, open a file and then start streaming to the file.
+			if (data.MementosRemaining != this.remaining--)
+			{
+				this.remaining = data.MementosRemaining;
+
+				if (this.stream != null)
+					this.stream.Dispose();
+
+				this.stream = this.snapshotter.Create(data.CurrentSequence, data.MementosRemaining);
+			}
+
+			this.stream.WriteItem(data.Serialized);
+			if (this.remaining != 0)
+				return;
+
+			this.stream.Dispose();
+			this.stream = null;
+			this.remaining = 0;
 		}
+
+		public SystemSnapshotHandler(SystemSnapshotter snapshotter)
+		{
+			this.snapshotter = snapshotter;
+		}
+
+		private readonly SystemSnapshotter snapshotter;
+		private SnapshotOutputStream stream;
+		private long remaining;
 	}
 }
---
+++ src/Hydrospanner/SystemSnapshotter.cs
@@ -0,0 +1,174 @@
+﻿namespace Hydrospanner
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Runtime.Remoting.Metadata.W3cXsd2001;
+	using System.Security.Cryptography;
+
+	public class SystemSnapshotter
+	{
+		public SnapshotInputStream Read()
+		{
+			var fullPaths = Directory.EnumerateFiles(this.path, this.prefix + "-*", SearchOption.TopDirectoryOnly).OrderByDescending(x => x).ToArray();
+
+			foreach (var fullPath in fullPaths)
+			{
+				var filename = Path.GetFileNameWithoutExtension(fullPath) ?? string.Empty;
+				var values = filename.Split("-".ToCharArray());
+				if (values.Length != 3)
+					continue;
+
+				var sequence = long.Parse(values[1]);
+				var hash = values[2];
+				var fileStream = File.OpenRead(fullPath);
+
+				var stream = SnapshotInputStream.Open(sequence, hash, fileStream);
+				if (stream != null)
+					return stream;
+			}
+
+			return new SnapshotInputStream();
+		}
+
+		public SnapshotOutputStream Create(long messageSequence, int itemCount)
+		{
+			var name = Format.FormatWith(this.prefix, messageSequence);
+			var fullname = Path.Combine(this.path, name);
+
+			var stream = new FileStream(fullname, FileMode.Create, FileAccess.Write, FileShare.Read, BufferSize, FileOptions.SequentialScan);
+			return new SnapshotOutputStream(stream, itemCount, checksum => File.Move(fullname, Format.FormatWith(fullname, checksum)));
+		}
+
+		public SystemSnapshotter(string path, string prefix)
+		{
+			this.path = path;
+			this.prefix = prefix;
+		}
+
+		private const string Format = "{0}-{1}";
+		private const int BufferSize = 1024 * 512;
+		private readonly string prefix;
+		private readonly string path;
+	}
+
+	public class SnapshotInputStream : IDisposable
+	{
+		public long Sequence
+		{
+			get { return this.sequence; }
+		}
+		public int ItemCount { get; private set; }
+
+		public IEnumerable<byte[]> Items
+		{
+			get
+			{
+				if (this.ItemCount == 0)
+					yield break;
+			}
+		}
+
+		public static SnapshotInputStream Open(long sequence, string hash, Stream stream)
+		{
+			hash = hash.Trim().ToUpperInvariant();
+			var computed = ComputeHash(stream).ToUpperInvariant();
+			stream.Position = 0;
+
+			if (hash == computed)
+				return new SnapshotInputStream(sequence, stream);
+
+			return null;
+		}
+		private static string ComputeHash(Stream stream)
+		{
+			using (var hasher = new SHA1Managed())
+			{
+				var computed = hasher.ComputeHash(stream);
+				return new SoapHexBinary(computed).ToString();
+			}
+		}
+
+		public SnapshotInputStream()
+		{
+		}
+		private SnapshotInputStream(long sequence, Stream stream)
+		{
+			this.sequence = sequence;
+			this.stream = stream;
+
+			var countBuffer = new byte[4];
+			stream.Read(countBuffer, 0, 4);
+			this.ItemCount = BitConverter.ToInt32(countBuffer, 0);
+		}
+
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+				this.stream.Dispose();
+		}
+
+		private readonly long sequence;
+		private readonly Stream stream;
+	}
+
+	public class SnapshotOutputStream : IDisposable
+	{
+		public void WriteItem(byte[] serialized)
+		{
+			if (this.complete == 0)
+				this.stream.Write(BitConverter.GetBytes(this.count), 0, 4); // total number of items in the snapshot
+
+			this.stream.Write(BitConverter.GetBytes(serialized.Length), 0, 4); // length of each item (not including this header)
+			this.stream.Write(serialized, 0, serialized.Length); // the item itself
+
+			if (++this.complete < this.count)
+				return; // more to do
+
+			this.stream.Position = 0;
+			var hash = ComputeHash(this.stream);
+
+			this.stream.Flush();
+			this.stream.Dispose();
+			
+			this.finalize(hash);
+		}
+		private static string ComputeHash(Stream stream)
+		{
+			using (var hasher = new SHA1Managed())
+			{
+				var computed = hasher.ComputeHash(stream);
+				return new SoapHexBinary(computed).ToString();
+			}
+		}
+
+		public SnapshotOutputStream(Stream stream, int count, Action<string> finalize)
+		{
+			this.stream = stream;
+			this.count = count;
+			this.finalize = finalize;
+		}
+
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+				this.stream.Dispose();
+		}
+
+		private readonly Stream stream;
+		private readonly int count;
+		private readonly Action<string> finalize;
+		private int complete;
+	}
+}
