5e0c74e2c377263de7716ae5ed708b0679832312
Whatcott, Michael <mike@smartystreets.com>
2013-03-11 16:07:01 -0600

    Included the IHydratable interfaces.

---
+++ src/proj/Hydrospanner/IHydratable.cs
@@ -0,0 +1,42 @@
+﻿namespace Hydrospanner
+{
+	using System.Collections.Generic;
+
+	public interface IHydratable
+	{
+		string Key { get; }
+		bool IsComplete { get; }
+		IEnumerable<object> GatherMessages();
+		object GetMemento();
+
+		void LoadFromMemento(object memento);
+		bool PublicSnapshot { get; }
+	}
+
+	public interface IHydratable<T>
+	{
+		void Hydrate(T message, Dictionary<string, string> headers, bool live);
+	}
+
+	public interface IHydratableKey
+	{
+		string Name { get; }
+		IHydratable Create();
+	}
+
+	public interface IHydratableSelector
+	{
+		IEnumerable<IHydratableKey> Keys(object message, Dictionary<string, string> headers = null);
+		IHydratable Create(object memento);
+	}
+
+	public interface IHydratableSelector<T>
+	{
+		IEnumerable<IHydratableKey> Keys(T message, Dictionary<string, string> headers = null);
+	}
+
+	public interface IHydratableFactory<T>
+	{
+		IHydratable Create(T memento);
+	}
+}
