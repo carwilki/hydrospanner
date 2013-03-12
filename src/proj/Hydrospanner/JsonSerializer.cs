a362b43108b9bebaef7900392b5cbb92fe8c7d65
Whatcott, Michael <mike@smartystreets.com>
2013-03-12 13:23:43 -0600

    Added json serializer settings.

--- src/proj/Hydrospanner/JsonSerializer.cs
+++ src/proj/Hydrospanner/JsonSerializer.cs
@@ -4,6 +4,7 @@
 	using System.Runtime.Serialization;
 	using System.Text;
 	using Newtonsoft.Json;
+	using Newtonsoft.Json.Converters;
 
 	internal class JsonSerializer
 	{
@@ -14,7 +15,7 @@
 			
 			try
 			{
-				var serialized = JsonConvert.SerializeObject(graph);
+				var serialized = JsonConvert.SerializeObject(graph, Settings);
 				return DefaultEncoding.GetBytes(serialized);
 			}
 			catch (Exception e)
@@ -26,9 +27,23 @@
 		public object Deserialize<T>(byte[] serialize)
 		{
 			var json = DefaultEncoding.GetString(serialize);
-			return JsonConvert.DeserializeObject<T>(json);
+			return JsonConvert.DeserializeObject<T>(json, Settings);
 		}
 
 		private static readonly Encoding DefaultEncoding = new UTF8Encoding(false);
+		private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+		{
+			NullValueHandling = NullValueHandling.Ignore,
+			TypeNameHandling = TypeNameHandling.None,
+			MissingMemberHandling = MissingMemberHandling.Ignore,
+			DefaultValueHandling = DefaultValueHandling.Ignore,
+			DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+			DateFormatHandling = DateFormatHandling.IsoDateFormat,
+			DateParseHandling = DateParseHandling.DateTime,
+			Converters = { new StringEnumConverter() },
+#if DEBUG
+			Formatting = Formatting.Indented
+#endif
+		};
 	}
 }
