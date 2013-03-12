9d33f07e377483c59a21404a6748c0e38d09a621
Whatcott, Michael <mike@smartystreets.com>
2013-03-12 10:24:55 -0600

    Created a place for extension methods.

---
+++ src/proj/Hydrospanner/Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Hydrospanner
+{
+	using System.Globalization;
+
+	public static class StringExtensions
+	{
+		public static string FormatWith(this string template, params object[] args)
+		{
+			return string.Format(CultureInfo.InvariantCulture, template, args);
+		}
+	}
+}
---
+++ src/proj/Hydrospanner/UnitTests/StringExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable 169
+// ReSharper disable InconsistentNaming
+
+namespace Hydrospanner.UnitTests
+{
+	using Machine.Specifications;
+
+	[Subject(typeof(StringExtensions))]
+	public class when_formatting_a_string
+	{
+		It should_function_like_the_string_format_method = () =>
+			"{0}-{1}".FormatWith(1, 2).ShouldEqual("1-2");
+	}
+}
+
+// ReSharper restore InconsistentNaming
+#pragma warning restore 169
