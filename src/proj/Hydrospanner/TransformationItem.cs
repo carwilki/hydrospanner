8f71f5736c1e55123fb7d6ba7850e44d54b8ea8e
Whatcott, Michael <mike@smartystreets.com>
2013-03-12 10:02:13 -0600

    Enabled package restore; included the TransformationItem.

---
+++ src/proj/Hydrospanner/TransformationItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Hydrospanner
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class TransformationItem
+	{
+		public long MessageSequence { get; private set; }
+		public byte[] SerializedBody { get; private set; }
+		public string SerializedType { get; private set; }
+		public byte[] SerializedHeaders { get; private set; }
+		public object Body { get; private set; }
+		public Dictionary<string, string> Headers { get; private set; }
+
+		public bool WriteToJournal { get; private set; }
+		public bool LiveMessage { get; private set; }
+		public bool DuplicateMessage { get; private set; }
+		public Guid WireId { get; private set; }
+		public Action AcknowledgeDelivery { get; private set; }
+	}
+}
