21c69d6758c476f579ca50c62a26bd9b2653fda2
Whatcott, Michael <mike@smartystreets.com>
2013-03-13 15:04:31 -0600

    Implemented the SnapshotLoader.

--- src/proj/Hydrospanner/Phases/Snapshot/SnapshotLoader.cs
+++ src/proj/Hydrospanner/Phases/Snapshot/SnapshotLoader.cs
@@ -1,17 +1,53 @@
 ﻿namespace Hydrospanner.Phases.Snapshot
 {
+	using System.IO;
 	using System.IO.Abstractions;
+	using System.Linq;
 
 	public class SnapshotLoader
 	{
-		public SnapshotLoader(DirectoryBase directory, string path, string prefix)
-		{
-		}
-
 		public SnapshotStreamReader LoadMostRecent()
 		{
-			// TODO: select most recently created viable snapshot and load it as a SnapshotStreamReader
-			return new SnapshotStreamReader();
+			return this.directory
+				.GetFiles(this.path, this.searchPattern, SearchOption.TopDirectoryOnly)
+				.OrderByDescending(x => x)
+				.Select(this.OpenOrBlank)
+				.FirstOrDefault(x => x.Count > 0) ?? new SnapshotStreamReader();
 		}
+
+		private SnapshotStreamReader OpenOrBlank(string fullPath)
+		{
+			var filename = Path.GetFileNameWithoutExtension(fullPath) ?? string.Empty;
+			var values = filename.Split(FieldDelimiter.ToCharArray());
+			if (values.Length != SnapshotFilenameFieldCount)
+				return new SnapshotStreamReader();
+
+			long sequence;
+			if (!long.TryParse(values[SequenceField], out sequence))
+				return new SnapshotStreamReader();
+
+			var hash = values[HashField];
+			var fileStream = this.file.OpenRead(fullPath);
+
+			return SnapshotStreamReader.Open(sequence, hash, fileStream) ?? new SnapshotStreamReader();
+		}
+
+		public SnapshotLoader(DirectoryBase directory, FileBase file, string path, string prefix)
+		{
+			this.directory = directory;
+			this.file = file;
+			this.path = path;
+			this.searchPattern = prefix + FieldDelimiter + WildcardPattern;
+		}
+
+		const int SequenceField = 1;
+		const int HashField = 2;
+		const int SnapshotFilenameFieldCount = 3;
+		const string FieldDelimiter = "-";
+		const string WildcardPattern = "*";
+		readonly DirectoryBase directory;
+		readonly FileBase file;
+		readonly string path;
+		readonly string searchPattern;
 	}
 }
--- src/tests/Hydrospanner.UnitTests/Phases/Snapshot/SnapshotLoaderTests.cs
+++ src/tests/Hydrospanner.UnitTests/Phases/Snapshot/SnapshotLoaderTests.cs
@@ -3,10 +3,12 @@
 
 namespace Hydrospanner.Phases.Snapshot
 {
-	using System.Collections.Generic;
+	using System;
 	using System.IO;
 	using System.IO.Abstractions;
-	using System.IO.Abstractions.TestingHelpers;
+	using System.Linq;
+	using System.Runtime.Remoting.Metadata.W3cXsd2001;
+	using System.Security.Cryptography;
 	using Machine.Specifications;
 	using NSubstitute;
 
@@ -21,43 +23,64 @@ namespace Hydrospanner.Phases.Snapshot
 					.Returns(new[] { "hi", "not-a-snapshot", "blah-blah-blah" });
 
 			It should_return_a_blank_snapshot = () =>
-				reader.ShouldNotBeNull(); // TODO: more verification here
+				reader.Count.ShouldEqual(0);
 		}
 
 		public class and_there_are_no_viable_snapshots
 		{
 			Establish context = () =>
+			{
+				file.OpenRead(Arg.Any<string>()).Returns(new MemoryStream(new byte[] { 0, 0, 0, 0 }));
 				directory
 					.GetFiles(Path, Arg.Is<string>(x => x.StartsWith(Prefix)), SearchOption.TopDirectoryOnly)
-					.Returns(new[] { Path + Prefix + "-bad_hash" });
+					.Returns(new[] { Path + Prefix + "-" + MessageSequence + "-bad_hash" });
+			};
 
-			It should_return_a_blank_snapshot;
+			It should_return_a_blank_snapshot = () =>
+				reader.Count.ShouldEqual(0);
 		}
 
-		public class and_there_are_viable_snapshots
+		public class and_there_is_at_least_one_viable_snapshot
 		{
 			Establish context = () =>
 			{
+				var oneRecord = BitConverter.GetBytes(1);
+				var firstRecordLength = BitConverter.GetBytes(4);
+				var contents = oneRecord.Concat(firstRecordLength).Concat(FirstRecord).ToArray();
+				var hash = new SoapHexBinary(new SHA1Managed().ComputeHash(contents));
+				var path = Path + Prefix + "-" + MessageSequence + "-" + hash;
 
+				directory
+					.GetFiles(Path, Arg.Is<string>(x => x.StartsWith(Prefix)), SearchOption.TopDirectoryOnly)
+					.Returns(new[] { path });
+				
+				file.OpenRead(path)
+					.Returns(new MemoryStream(contents));
 			};
 
-			It should_load_the_most_recent_snapshot;
+			It should_load_the_most_recent_viable_snapshot = () =>
+				reader.Read().First().ShouldBeLike(FirstRecord);
+
+			static readonly byte[] FirstRecord = BitConverter.GetBytes(42);
 		}
 
 		Establish context = () =>
 		{
+			file = Substitute.For<FileBase>();
 			directory = Substitute.For<DirectoryBase>();
-			loader = new SnapshotLoader(directory, Path, Prefix);
+			loader = new SnapshotLoader(directory, file, Path, Prefix);
 		};
 
 		Because of = () =>
 			reader = loader.LoadMostRecent();
 
-		const string Path = "./path/to/snapshots";
-		const string Prefix = "prefix";
+		const string Path = "./path/to/snapshots/";
+		const string Prefix = "1";
+		const int MessageSequence = 42;
 		static SnapshotLoader loader;
 		static SnapshotStreamReader reader;
 		static DirectoryBase directory;
+		static FileBase file;
 	}
 }
 
