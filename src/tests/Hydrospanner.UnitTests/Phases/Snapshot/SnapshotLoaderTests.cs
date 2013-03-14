f410e4ed5d0605139f3024f1668067818034f8b9
Whatcott, Michael <mike@smartystreets.com>
2013-03-14 12:56:01 -0600

    Consolidated preparation of test snapshot.

--- src/tests/Hydrospanner.UnitTests/Phases/Snapshot/SnapshotLoaderTests.cs
+++ src/tests/Hydrospanner.UnitTests/Phases/Snapshot/SnapshotLoaderTests.cs
@@ -51,16 +51,7 @@ namespace Hydrospanner.Phases.Snapshot
 		{
 			Establish context = () =>
 			{
-				var oneRecord = BitConverter.GetBytes(1);
-				var firstRecordType = Encoding.UTF8.GetBytes(FirstRecord.GetType().AssemblyQualifiedName ?? string.Empty);
-				var firstRecordTypeLength = BitConverter.GetBytes(firstRecordType.Length);
-				var firstRecordLength = BitConverter.GetBytes(4);
-				var contents = oneRecord
-					.Concat(firstRecordTypeLength)
-					.Concat(firstRecordType)
-					.Concat(firstRecordLength)
-					.Concat(FirstRecord).ToArray();
-				var hash = new SoapHexBinary(new SHA1Managed().ComputeHash(contents));
+				
 				var earlierPath = Path + EarlierIteration + "-" + MessageSequence + "-" + hash;
 				var laterPath = Path + LaterIteration + "-" + MessageSequence + "-" + hash;
 
@@ -80,8 +71,71 @@ namespace Hydrospanner.Phases.Snapshot
 				reader.Iteration.ShouldEqual(int.Parse(LaterIteration));
 				reader.Read().First().Value.ShouldBeLike(FirstRecord);
 			};
+		}
 
-			static readonly byte[] FirstRecord = BitConverter.GetBytes(42);
+		public class when_loading_snapshots_using_parameterized_constraints
+		{
+			public class when_loading_a_snapshot_based_on_message_sequence
+			{
+				Establish context = () =>
+				{
+					var earlierPath = Path + EarlierIteration + "-" + EarlySnapshotSequence + "-" + hash;
+					var laterPath = Path + EarlierIteration + "-" + LaterSnapshotSequence + "-" + hash;
+
+					directory
+						.GetFiles(Path, "*", SearchOption.TopDirectoryOnly)
+						.Returns(new[] { laterPath, earlierPath });
+
+					file.OpenRead(earlierPath).Returns(new MemoryStream(contents));
+					file.OpenRead(laterPath).Returns(new MemoryStream(contents));
+				};
+
+				Because of = () =>
+					reader = loader.Load(StoredMessageSequence);
+
+				It should_load_the_snapshot_whose_message_sequence_is_at_or_lower_the_provided_sequence = () =>
+				{
+					reader.MessageSequence.ShouldEqual(EarlySnapshotSequence);
+					reader.Read().First().Value.ShouldBeLike(FirstRecord);
+				};
+
+				const long EarlySnapshotSequence = StoredMessageSequence;
+				const long LaterSnapshotSequence = StoredMessageSequence + 1;
+			}
+
+			public class when_loading_a_snapshot_based_on_snapshot_iteration
+			{
+				Establish context = () =>
+				{
+					var earlierPath = Path + EarlierIteration + "-" + StoredMessageSequence + "-" + hash;
+					var laterPath = Path + LaterIteration + "-" + StoredMessageSequence + "-" + hash;
+
+					directory
+						.GetFiles(Path, "*", SearchOption.TopDirectoryOnly)
+						.Returns(new[] { earlierPath, laterPath });
+
+					file.OpenRead(earlierPath).Returns(x => { throw new DivideByZeroException("This code should NOT be executed!!"); });
+					file.OpenRead(laterPath).Returns(new MemoryStream(contents));
+				};
+
+				Because of = () =>
+					reader = loader.Load(long.MaxValue);
+
+				It should_ignore_snapshots_with_iterations_lower_than_the_provided_iteration = () =>
+				{
+					reader.Iteration.ShouldEqual(int.Parse(LaterIteration));
+					reader.Read().First().Value.ShouldBeLike(FirstRecord);
+				};
+			}
+
+			Establish context = () =>
+			{
+				file = Substitute.For<FileBase>();
+				directory = Substitute.For<DirectoryBase>();
+				loader = new SnapshotLoader(directory, file, Path);
+			};
+
+			const int StoredMessageSequence = 42;
 		}
 
 		Establish context = () =>
@@ -89,111 +143,33 @@ namespace Hydrospanner.Phases.Snapshot
 			file = Substitute.For<FileBase>();
 			directory = Substitute.For<DirectoryBase>();
 			loader = new SnapshotLoader(directory, file, Path);
+
+			var oneRecord = BitConverter.GetBytes(1);
+			var firstRecordType = Encoding.UTF8.GetBytes(FirstRecord.GetType().AssemblyQualifiedName ?? string.Empty);
+			var firstRecordTypeLength = BitConverter.GetBytes(firstRecordType.Length);
+			var firstRecordLength = BitConverter.GetBytes(4);
+			contents = oneRecord
+				.Concat(firstRecordTypeLength)
+				.Concat(firstRecordType)
+				.Concat(firstRecordLength)
+				.Concat(FirstRecord).ToArray();
+			hash = new SoapHexBinary(new SHA1Managed().ComputeHash(contents)).ToString();
 		};
 
 		const string Path = "./path/to/snapshots/";
 		const string EarlierIteration = "1";
 		const string LaterIteration = "2";
 		const int MessageSequence = 42;
-		static SnapshotLoader loader;
-		static SnapshotStreamReader reader;
-		static DirectoryBase directory;
-		static FileBase file;
-	}
-
-	[Subject(typeof(SnapshotLoader))]
-	public class when_loading_snapshots_using_parameterized_constraints
-	{
-		public class when_loading_a_snapshot_based_on_message_sequence
-		{
-			Establish context = () =>
-			{
-				var oneRecord = BitConverter.GetBytes(1);
-				var firstRecordType = Encoding.UTF8.GetBytes(FirstRecord.GetType().AssemblyQualifiedName ?? string.Empty);
-				var firstRecordTypeLength = BitConverter.GetBytes(firstRecordType.Length);
-				var firstRecordLength = BitConverter.GetBytes(4);
-				var contents = oneRecord
-					.Concat(firstRecordTypeLength)
-					.Concat(firstRecordType)
-					.Concat(firstRecordLength)
-					.Concat(FirstRecord).ToArray();
-				var hash = new SoapHexBinary(new SHA1Managed().ComputeHash(contents));
-				var earlierPath = Path + EarlierIteration + "-" + EarlySnapshotSequence + "-" + hash;
-				var laterPath = Path + EarlierIteration + "-" + LaterSnapshotSequence + "-" + hash;
-
-				directory
-					.GetFiles(Path, "*", SearchOption.TopDirectoryOnly)
-					.Returns(new[] { laterPath, earlierPath });
-
-				file.OpenRead(earlierPath).Returns(new MemoryStream(contents));
-				file.OpenRead(laterPath).Returns(new MemoryStream(contents));
-			};
-
-			Because of = () =>
-				reader = loader.Load(StoredMessageSequence);
-
-			It should_load_the_snapshot_whose_message_sequence_is_at_or_lower_the_provided_sequence = () =>
-			{
-				reader.MessageSequence.ShouldEqual(EarlySnapshotSequence);
-				reader.Read().First().Value.ShouldBeLike(FirstRecord);
-			};
-
-			const long EarlySnapshotSequence = StoredMessageSequence;
-			const long LaterSnapshotSequence = StoredMessageSequence + 1;
-		}
-
-		public class when_loading_a_snapshot_based_on_snapshot_iteration
-		{
-			Establish context = () =>
-			{
-				var oneRecord = BitConverter.GetBytes(1);
-				var firstRecordType = Encoding.UTF8.GetBytes(FirstRecord.GetType().AssemblyQualifiedName ?? string.Empty);
-				var firstRecordTypeLength = BitConverter.GetBytes(firstRecordType.Length);
-				var firstRecordLength = BitConverter.GetBytes(4);
-				var contents = oneRecord
-					.Concat(firstRecordTypeLength)
-					.Concat(firstRecordType)
-					.Concat(firstRecordLength)
-					.Concat(FirstRecord).ToArray();
-				var hash = new SoapHexBinary(new SHA1Managed().ComputeHash(contents));
-				var earlierPath = Path + EarlierIteration + "-" + StoredMessageSequence + "-" + hash;
-				var laterPath = Path + LaterIteration + "-" + StoredMessageSequence + "-" + hash;
-
-				directory
-					.GetFiles(Path, "*", SearchOption.TopDirectoryOnly)
-					.Returns(new[] { earlierPath, laterPath });
-
-				file.OpenRead(earlierPath).Returns(x => { throw new DivideByZeroException("This code should NOT be executed!!"); });
-				file.OpenRead(laterPath).Returns(new MemoryStream(contents));
-			};
-
-			Because of = () =>
-				reader = loader.Load(long.MaxValue);
-
-			It should_ignore_snapshots_with_iterations_lower_than_the_provided_iteration = () =>
-			{
-				reader.Iteration.ShouldEqual(int.Parse(LaterIteration));
-				reader.Read().First().Value.ShouldBeLike(FirstRecord);
-			};
-		}
-
-		Establish context = () =>
-		{
-			file = Substitute.For<FileBase>();
-			directory = Substitute.For<DirectoryBase>();
-			loader = new SnapshotLoader(directory, file, Path);
-		};
-
-		const string Path = "./path/to/snapshots/";
-		const string EarlierIteration = "1";
-		const string LaterIteration = "2";
-		const int StoredMessageSequence = 42;
 		static readonly byte[] FirstRecord = BitConverter.GetBytes(42);
 		static SnapshotLoader loader;
 		static SnapshotStreamReader reader;
 		static DirectoryBase directory;
 		static FileBase file;
+		static byte[] contents;
+		static string hash;
 	}
+
+	
 }
 
 // ReSharper restore InconsistentNaming
