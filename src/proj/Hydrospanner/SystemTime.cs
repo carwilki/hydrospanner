81f1ec0fc5526caae924653d1d417098be5bceac
Oliver, Jonathan <joliver@localhost>
2013-03-14 10:22:28 -0600

    Added SystemTime.

---
+++ src/proj/Hydrospanner/SystemTime.cs
@@ -0,0 +1,42 @@
+﻿namespace Hydrospanner
+{
+	using System;
+
+	public static class SystemTime
+	{
+		public static DateTime UtcNow
+		{
+			get
+			{
+				if (cycle == null)
+					return DateTime.UtcNow;
+
+				if (index >= cycle.Length)
+					index = 0;
+
+				return cycle[index++];
+			}
+		}
+
+		public static void Freeze(params DateTime[] values)
+		{
+			index = 0;
+			cycle = values == null || values.Length == 0 ? new[] { DateTime.UtcNow } : values;
+		}
+
+		public static void Unfreeze()
+		{
+			index = 0;
+			cycle = null;
+		}
+
+		public static int EpochUtcNow { get { return (int)((UtcNow.Ticks - 621355968000000000) / 10000000); } }
+
+		private static int index;
+		private static DateTime[] cycle;
+	}
+
+	// Credits:
+	// http://ayende.com/blog/3408/dealing-with-time-in-tests
+	// http://stackoverflow.com/questions/2425721/unit-testing-datetime-now
+}
---
+++ src/tests/Hydrospanner.UnitTests/SystemTimeTests.cs
@@ -0,0 +1,119 @@
+﻿#pragma warning disable 169
+// ReSharper disable InconsistentNaming
+
+namespace Hydrospanner
+{
+	using System;
+	using Machine.Specifications;
+
+	[Subject(typeof(SystemTime))]
+	public class when_generating_utc_now
+	{
+		It should_reflect_the_actual_clock_time = () =>
+		{
+			for (var i = 0; i < 100; i++)
+				SystemTime.UtcNow.ShouldBeCloseTo(DateTime.UtcNow, TimeSpan.FromMilliseconds(1));
+		};
+	}
+
+	[Subject(typeof(SystemTime))]
+	public class when_freezing_system_time
+	{
+		Establish context = () =>
+			SystemTime.Freeze(DateTime.MinValue, DateTime.MaxValue);
+
+		It should_loop_continually_through_the_given_datetime_values = () =>
+		{
+			SystemTime.UtcNow.ShouldEqual(DateTime.MinValue);
+			SystemTime.UtcNow.ShouldEqual(DateTime.MaxValue);
+
+			SystemTime.UtcNow.ShouldEqual(DateTime.MinValue);
+			SystemTime.UtcNow.ShouldEqual(DateTime.MaxValue);
+		};
+
+		Cleanup after = () =>
+			SystemTime.Unfreeze();
+	}
+
+	[Subject(typeof(SystemTime))]
+	public class when_freezing_with_no_specified_dates
+	{
+		Because of = () =>
+		{
+			justBeforeFreeze = SystemTime.UtcNow;
+			SystemTime.Freeze();
+			afterFreeze = SystemTime.UtcNow;
+			afterFreeze2 = SystemTime.UtcNow;
+		};
+
+		It should_freeze_at_the_current_time = () =>
+			afterFreeze.ShouldBeCloseTo(justBeforeFreeze, TimeSpan.FromMilliseconds(10));
+
+		It should_stay_frozen = () =>
+			afterFreeze.ShouldEqual(afterFreeze2);
+
+		Cleanup after = () =>
+			SystemTime.Unfreeze();
+
+		static DateTime justBeforeFreeze;
+		static DateTime afterFreeze;
+		static DateTime afterFreeze2;
+	}
+
+	[Subject(typeof(SystemTime))]
+	public class when_freezing_with_a_null_collection
+	{
+		Because of = () =>
+		{
+			justBeforeFreeze = SystemTime.UtcNow;
+			SystemTime.Freeze(null);
+			afterFreeze = SystemTime.UtcNow;
+			afterFreeze2 = SystemTime.UtcNow;
+		};
+
+		It should_freeze_at_the_current_time = () =>
+			afterFreeze.ShouldBeCloseTo(justBeforeFreeze, TimeSpan.FromMilliseconds(10));
+
+		It should_stay_frozen = () =>
+			afterFreeze.ShouldEqual(afterFreeze2);
+
+		Cleanup after = () =>
+			SystemTime.Unfreeze();
+
+		static DateTime justBeforeFreeze;
+		static DateTime afterFreeze;
+		static DateTime afterFreeze2;
+	}
+
+	[Subject(typeof(SystemTime))]
+	public class when_time_has_been_frozen_and_then_unfrozen
+	{
+		Establish context = () =>
+			SystemTime.Freeze(DateTime.MinValue);
+
+		Because of = () =>
+			SystemTime.Unfreeze();
+
+		It should_reflect_the_actual_clock_time = () =>
+			SystemTime.UtcNow.ShouldBeCloseTo(DateTime.UtcNow, TimeSpan.FromMilliseconds(1));
+	}
+
+	[Subject(typeof(SystemTime))]
+	public class when_requesting_epoch_time
+	{
+		Establish context = () =>
+			SystemTime.Freeze(Value);
+
+		It should_be_derived_from_the_current_time = () =>
+			SystemTime.EpochUtcNow.ShouldEqual(ExpectedEpochValue);
+
+		Cleanup after = () =>
+			SystemTime.Unfreeze();
+
+		const int ExpectedEpochValue = 1360881313;
+		static readonly DateTime Value = DateTime.Parse("2/14/2013 10:35:13 PM");
+	}
+}
+
+// ReSharper restore InconsistentNaming
+#pragma warning restore 169
