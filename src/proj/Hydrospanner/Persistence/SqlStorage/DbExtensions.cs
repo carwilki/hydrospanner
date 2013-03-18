26d192a6d6f9b4f60d254c772946da806618a99b
Oliver, Jonathan <joliver@localhost>
2013-03-18 13:21:24 -0600

    Stubbed out simple implementation of message storage.

---
+++ src/proj/Hydrospanner/Persistence/SqlStorage/DbExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Hydrospanner.Persistence.SqlStorage
+{
+	using System.Data;
+	using System.Data.Common;
+
+	internal static class DbExtensions
+	{
+		public static IDbConnection OpenConnection(this DbProviderFactory factory, string connectionString)
+		{
+			var connection = factory.CreateConnection();
+			if (connection == null)
+				return null;
+
+			try
+			{
+				connection.ConnectionString = connectionString;
+				connection.Open();
+				return connection;
+			}
+			catch
+			{
+				connection.Dispose();
+				return null;
+			}
+		}
+	}
+}
