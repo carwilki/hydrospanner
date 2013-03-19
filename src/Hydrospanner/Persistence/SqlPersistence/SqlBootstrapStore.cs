b9d20b556455724f77517cb42c67e6c3282e752c
Oliver, Jonathan <joliver@localhost>
2013-03-19 11:32:02 -0600

    Added storage to facilitate bootstrapping.

---
+++ src/Hydrospanner/Persistence/SqlPersistence/SqlBootstrapStore.cs
@@ -0,0 +1,10 @@
+﻿namespace Hydrospanner.Persistence.SqlPersistence
+{
+	public class SqlBootstrapStore : IBootstrapStore
+	{
+		public BootstrapInfo Load()
+		{
+			return default(BootstrapInfo);
+		}
+	}
+}
--- src/Hydrospanner/Persistence/SqlPersistence/SqlMessageStore.cs
+++ src/Hydrospanner/Persistence/SqlPersistence/SqlMessageStore.cs
@@ -1,13 +1,34 @@
 ﻿namespace Hydrospanner.Persistence.SqlPersistence
 {
+	using System;
 	using System.Collections.Generic;
+	using System.Data.Common;
 	using Hydrospanner.Phases.Journal;
 
 	public class SqlMessageStore : IMessageStore
 	{
 		public bool Save(List<JournalItem> items)
 		{
+			if (items == null || items.Count == 0)
+				return true;
+
 			return false;
 		}
+
+		public SqlMessageStore(DbProviderFactory factory, string connectionString)
+		{
+			if (factory == null)
+				throw new ArgumentNullException("factory");
+			
+			if (string.IsNullOrWhiteSpace(connectionString))
+				throw new ArgumentNullException("connectionString");
+
+			this.factory = factory;
+			this.connectionString = connectionString;
+		}
+
+		private static readonly TimeSpan Timeout = TimeSpan.FromSeconds(5);
+		private readonly DbProviderFactory factory;
+		private readonly string connectionString;
 	}
 }
