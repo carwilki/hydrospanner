cbd0c0a3c363bda5c6cf65082d832856a59c4d1e
Oliver, Jonathan <joliver@localhost>
2013-03-19 13:26:05 -0600

    Added implementation to facilitate checkpoints.

---
+++ src/Hydrospanner/Persistence/SqlPersistence/SqlCheckpointStore.cs
@@ -0,0 +1,49 @@
+﻿namespace Hydrospanner.Persistence.SqlPersistence
+{
+	using System;
+	using System.Data.Common;
+
+	public class SqlCheckpointStore : IDispatchCheckpointStore
+	{
+		public void Save(long sequence)
+		{
+			if (sequence <= 0)
+				return;
+
+			while (true)
+			{
+				try
+				{
+					this.TrySave(sequence);
+				}
+				catch
+				{
+					Timeout.Sleep();
+				}	
+			}
+		}
+		private void TrySave(long sequence)
+		{
+			using (var connection = this.factory.OpenConnection(this.connectionString))
+			using (var command = connection.CreateCommand(SqlStatement.FormatWith(sequence)))
+				command.ExecuteNonQuery();
+		}
+
+		public SqlCheckpointStore(DbProviderFactory factory, string connectionString)
+		{
+			if (factory == null)
+				throw new ArgumentNullException("factory");
+			
+			if (string.IsNullOrWhiteSpace(connectionString))
+				throw new ArgumentNullException("connectionString");
+
+			this.factory = factory;
+			this.connectionString = connectionString;
+		}
+
+		private const string SqlStatement = @"UPDATE checkpoints SET dispatch = {0} WHERE {0} > dispatch;";
+		private static readonly TimeSpan Timeout = TimeSpan.FromSeconds(5);
+		private readonly DbProviderFactory factory;
+		private readonly string connectionString;
+	}
+}
