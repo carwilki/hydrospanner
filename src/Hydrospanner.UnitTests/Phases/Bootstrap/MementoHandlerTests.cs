adb99b69d4ed90bfa73ba5307b4e00944386fc8a
Oliver, Jonathan <joliver@localhost>
2013-03-21 16:11:49 -0600

    MementoHandler is now under test.

---
+++ src/Hydrospanner.UnitTests/Phases/Bootstrap/MementoHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿#pragma warning disable 169
+// ReSharper disable InconsistentNaming
+
+namespace Hydrospanner.Phases.Bootstrap
+{
+	using System;
+	using Machine.Specifications;
+	using NSubstitute;
+
+	[Subject(typeof(MementoHandler))]
+	public class when_a_null_repository_is_provided
+	{
+		It should_throw_an_exception = () =>
+			Catch.Exception(() => new MementoHandler(null)).ShouldBeOfType<ArgumentNullException>();
+	}
+
+	public class when_a_bootstrap_item_is_handled
+	{
+		Establish context = () =>
+		{
+			item = new BootstrapItem { Memento = 42 };
+			repository = Substitute.For<IRepository>();
+			handler = new MementoHandler(repository);
+		};
+
+		Because of = () =>
+			handler.OnNext(item, 0, false);
+
+		It should_pass_the_memento_to_the_repository = () =>
+			repository.Received(1).Restore(item.Memento);
+
+		static BootstrapItem item;
+		static IRepository repository;
+		static MementoHandler handler;
+	}
+}
+
+// ReSharper restore InconsistentNaming
+#pragma warning restore 169
--- src/Hydrospanner/Phases/Bootstrap/MementoHandler.cs
+++ src/Hydrospanner/Phases/Bootstrap/MementoHandler.cs
@@ -1,16 +1,20 @@
 ﻿namespace Hydrospanner.Phases.Bootstrap
 {
+	using System;
 	using Disruptor;
 
 	public class MementoHandler : IEventHandler<BootstrapItem>
 	{
 		public void OnNext(BootstrapItem data, long sequence, bool endOfBatch)
 		{
-			this.repository.Restore(data.Memento); // TODO: under test
+			this.repository.Restore(data.Memento);
 		}
 
 		public MementoHandler(IRepository repository)
 		{
+			if (repository == null)
+				throw new ArgumentNullException("repository");
+
 			this.repository = repository;
 		}
 
