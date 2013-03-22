72bf97ea57857382d84de0ef4f9f34b6173fcbd0
Oliver, Jonathan <joliver@localhost>
2013-03-22 15:26:25 -0600

    Revised DisruptorFactory to depend upon a few other factories.

--- src/Hydrospanner/Configuration/DisruptorFactory.cs
+++ src/Hydrospanner/Configuration/DisruptorFactory.cs
@@ -24,24 +24,12 @@
 
 			return new DisruptorBase<BootstrapItem>(disruptor);
 		}
-		public virtual IDisruptor<TransformationItem> CreateStartupTransformationDisruptor(BootstrapInfo info, IRepository repository, Action complete)
+
+		public virtual IDisruptor<JournalItem> CreateJournalDisruptor(BootstrapInfo info)
 		{
-			var countdown = info.JournaledSequence - info.SnapshotSequence;
-			var transformationHandler = new TransformationHandler(); // TODO
-
-			var disruptor = CreateDisruptor<TransformationItem>(new YieldingWaitStrategy(), 1024 * 128);
-			disruptor.HandleEventsWith(new DeserializationHandler(CreateSerializer()))
-			    .Then(transformationHandler)
-			    .Then(new CoutdownHandler(countdown, complete));
-
-			return new DisruptorBase<TransformationItem>(disruptor);
-		}
-
-		public virtual IDisruptor<JournalItem> CreateJournalDisruptor(BootstrapInfo info, MessagingFactory messaging, PersistenceFactory persistence)
-		{
-			var messageStore = persistence.CreateMessageStore(info.SerializedTypes);
-			var messageSender = messaging.CreateMessageSender();
-			var checkpointStore = persistence.CreateDispatchCheckpointStore();
+			var messageStore = this.persistence.CreateMessageStore(info.SerializedTypes);
+			var messageSender = this.messaging.CreateMessageSender();
+			var checkpointStore = this.persistence.CreateDispatchCheckpointStore();
 
 			var disruptor = CreateDisruptor<JournalItem>(new SleepingWaitStrategy(), 1024 * 64);
 			disruptor.HandleEventsWith(new Phases.Journal.SerializationHandler(new JsonSerializer()))
@@ -49,24 +37,38 @@
 			    .Then(new AcknowledgmentHandler(), new DispatchHandler(messageSender))
 			    .Then(new DispatchCheckpointHandler(checkpointStore));
 
+			this.journalRing = disruptor.RingBuffer;
 			return new DisruptorBase<JournalItem>(disruptor);
 		}
-		public virtual IDisruptor<SnapshotItem> CreateSnapshotDisruptor(SnapshotFactory factory)
+		public virtual IDisruptor<SnapshotItem> CreateSnapshotDisruptor()
 		{
-			var systemRecorder = factory.CreateSystemSnapshotRecorder();
-			var publicRecorder = factory.CreatePublicSnapshotRecorder();
+			var systemRecorder = this.snapshots.CreateSystemSnapshotRecorder();
+			var publicRecorder = this.snapshots.CreatePublicSnapshotRecorder();
 
 			var disruptor = CreateDisruptor<SnapshotItem>(new SleepingWaitStrategy(), 1024 * 8);
 			disruptor.HandleEventsWith(new Phases.Snapshot.SerializationHandler(CreateSerializer()))
-			    .Then(new SystemSnapshotHandler(systemRecorder, factory.SnapshotGeneration), new PublicSnapshotHandler(publicRecorder));
+			    .Then(new SystemSnapshotHandler(systemRecorder, this.snapshots.SnapshotGeneration), new PublicSnapshotHandler(publicRecorder));
 
+			this.snapshotRing = disruptor.RingBuffer;
 			return new DisruptorBase<SnapshotItem>(disruptor);
+		}
+
+		public virtual IDisruptor<TransformationItem> CreateStartupTransformationDisruptor(IRepository repository, BootstrapInfo info, Action complete)
+		{
+			var countdown = info.JournaledSequence - info.SnapshotSequence;
+			this.transformationHandler = new TransformationHandler(info.JournaledSequence, this.snapshotRing, this.journalRing);
+
+			var disruptor = CreateDisruptor<TransformationItem>(new YieldingWaitStrategy(), 1024 * 128);
+			disruptor.HandleEventsWith(this.serializationHandler)
+			    .Then(this.transformationHandler)
+			    .Then(new CoutdownHandler(countdown, complete));
+
+			return new DisruptorBase<TransformationItem>(disruptor);
 		}
 		public virtual IDisruptor<TransformationItem> CreateTransformationDisruptor()
 		{
-			var transformationHandler = new TransformationHandler(); // TODO
 			var disruptor = CreateDisruptor<TransformationItem>(new SleepingWaitStrategy(), 1024 * 128);
-			disruptor.HandleEventsWith(new DeserializationHandler(CreateSerializer())).Then(transformationHandler);
+			disruptor.HandleEventsWith(this.serializationHandler).Then(this.transformationHandler);
 			return new DisruptorBase<TransformationItem>(disruptor);
 		}
 
@@ -74,10 +76,28 @@
 		{
 			return new JsonSerializer();
 		}
-
 		private static Disruptor<T> CreateDisruptor<T>(IWaitStrategy wait, int size) where T : class, new()
 		{
 			return new Disruptor<T>(() => new T(), new SingleThreadedClaimStrategy(size), wait, TaskScheduler.Default);
 		}
+
+		public DisruptorFactory(MessagingFactory messaging, PersistenceFactory persistence, SnapshotFactory snapshots)
+		{
+			this.messaging = messaging;
+			this.snapshots = snapshots;
+			this.persistence = persistence;
+		}
+		protected DisruptorFactory()
+		{
+		}
+
+		private readonly DeserializationHandler serializationHandler = new DeserializationHandler(CreateSerializer());
+		private readonly SnapshotFactory snapshots;
+		private readonly MessagingFactory messaging;
+		private readonly PersistenceFactory persistence;
+
+		private TransformationHandler transformationHandler;
+		private RingBuffer<JournalItem> journalRing;
+		private RingBuffer<SnapshotItem> snapshotRing;
 	}
 }
--- src/Hydrospanner/Phases/Transformation/TransformationHandler.cs
+++ src/Hydrospanner/Phases/Transformation/TransformationHandler.cs
@@ -1,6 +1,8 @@
 ﻿namespace Hydrospanner.Phases.Transformation
 {
 	using Disruptor;
+	using Journal;
+	using Snapshot;
 
 	public sealed class TransformationHandler : IEventHandler<TransformationItem>
 	{
@@ -16,6 +18,10 @@
 			// for each message in the buffer, push the message in and gather out any resulting messages which are then added to the local buffer
 			// and so on until no messages are resulting.
 			// then take that entire set of messages (all of which resulted from an incoming message) and push to the next phase as a batch
+		}
+
+		public TransformationHandler(long journaledSequence, RingBuffer<SnapshotItem> snapshotRing, RingBuffer<JournalItem> journalRing)
+		{
 		}
 	}
 }
