b1ff9daf6de512a877b02bf046afa0740a989162
Oliver, Jonathan <joliver@localhost>
2013-03-22 17:04:27 -0600

    Added IRepo method.

--- src/Hydrospanner/IRepository.cs
+++ src/Hydrospanner/IRepository.cs
@@ -2,8 +2,11 @@
 {
 	using System.Collections.Generic;
 
+	// NOTE: this implementation of IRepo keeps track of tombstoned objects which is extremely convenient because
+	// when we restore the state of the system from snapshots, it can just look for a special memento that it understands and restore it...
 	public interface IRepository
 	{
+		IEnumerable<object> GetMemento();
 		IEnumerable<IHydratable> Load(object message, Dictionary<string, string> headers);
 		void Delete(IHydratable hydratable);
 		void Restore(object memento);
