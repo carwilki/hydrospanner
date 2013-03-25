c871215ce59a79ecd7d5a8bbb0dcfa8cdc689637
Oliver, Jonathan <joliver@localhost>
2013-03-25 11:34:58 -0600

    Added MessageBootstrapper TODOs.

--- src/Hydrospanner/Phases/Bootstrap/MessageBootstrapper.cs
+++ src/Hydrospanner/Phases/Bootstrap/MessageBootstrapper.cs
@@ -6,6 +6,18 @@
 	{
 		public virtual void Restore(BootstrapInfo info, object snapshotRing, object journalRing, IRepository repository)
 		{
+			// TODO: standard null ref checks/throw
+
+			// TODO: read all messages from info.DispatchSequence || info.SnapshotSequence (whichever is less)
+
+			// for each message
+			// if message.Seq > info.DispatchSequence, push to dispatch ring (it hasn't been dispatched yet)
+
+			// potentially need to create CreateStartupTransformationDisruptor (and start)
+			// if message.Seq > info.SnapshotSequence, push to transformation ring
+
+			// once all messages pushed to either dispatch or startup transformation, shut down startup transformation disruptor
+
 			throw new System.NotImplementedException();
 		}
 	}
