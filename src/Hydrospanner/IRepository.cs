c93e41630612f85c4dfc29c79645038794b09407
Whatcott, Michael <mike@smartystreets.com>
2013-03-27 16:36:27 -0600

    Spike draft of the Transformer.

--- src/Hydrospanner/IRepository.cs
+++ src/Hydrospanner/IRepository.cs
@@ -2,8 +2,9 @@
 {
 	using System.Collections.Generic;
 
-	// NOTE: this implementation of IRepo keeps track of tombstoned objects which is extremely convenient because
-	// when we restore the state of the system from snapshots, it can just look for a special memento that it understands and restore it...
+	// NOTE: this implementation of IRepo keeps track of tomb-stoned objects which is extremely convenient because when we 
+	// restore the state of the system from snapshots, it can just look for a special memento that it understands and restore it...
+	
 	public interface IRepository
 	{
 		IEnumerable<object> GetMemento();
--- src/Hydrospanner/Phases/Transformation/Transformer.cs
+++ src/Hydrospanner/Phases/Transformation/Transformer.cs
@@ -1,6 +1,7 @@
 ﻿namespace Hydrospanner.Phases.Transformation
 {
 	using System.Collections.Generic;
+	using System.Linq;
 	using Snapshot;
 
 	public interface ITransformer
@@ -10,18 +11,43 @@
 
 	public class Transformer : ITransformer
 	{
+		// TODO: delete and TDD; this is just spike code to see if I've got the concepts down...
+
 		public IEnumerable<object> Handle(object message, Dictionary<string, string> headers, bool live)
 		{
-			yield break;
+			this.currentSequence++;
+
+			var hydratables = this.repository.Load(message, headers);
+
+			foreach (var h in hydratables.Where(h => !h.IsComplete))
+			{
+				h.Hydrate(message, headers, live);
+
+				if (h.IsPublicSnapshot || h.IsComplete)
+				{
+					var next = this.snapshotRing.Next();
+					var claimed = this.snapshotRing[next];
+					claimed.AsPublicSnapshot(h.Key, h.GetMemento(), this.currentSequence);
+					this.snapshotRing.Publish(next);
+					
+					if (h.IsComplete) // Not sure about this...
+						this.repository.Delete(h);
+				}
+
+				foreach (var m in h.GatherMessages())
+					yield return m;
+			}
 		}
 
-		public Transformer(IRepository repository, IRingBuffer<SnapshotItem> snapshotRing)
+		public Transformer(IRepository repository, IRingBuffer<SnapshotItem> snapshotRing, long journaledSequence)
 		{
 			this.snapshotRing = snapshotRing;
+			this.currentSequence = journaledSequence + 1;
 			this.repository = repository;
 		}
 
 		readonly IRepository repository;
 		readonly IRingBuffer<SnapshotItem> snapshotRing;
+		long currentSequence;
 	}
 }
