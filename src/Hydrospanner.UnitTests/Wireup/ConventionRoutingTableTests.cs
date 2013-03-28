fb397bf38063eab24571818ca438a247eb9d13ec
Oliver, Jonathan <jonathan@localhost>
2013-03-28 16:00:51 -0600

    Stubbed out ConventionRoutingTable.

---
+++ src/Hydrospanner.UnitTests/Wireup/ConventionRoutingTableTests.cs
@@ -0,0 +1,62 @@
+﻿#pragma warning disable 169
+// ReSharper disable InconsistentNaming
+
+namespace Hydrospanner.Wireup
+{
+	using System.Collections.Generic;
+	using Machine.Specifications;
+
+	[Subject(typeof(ConventionRoutingTable))]
+	public class when_routing_by_convention
+	{
+		public class when_providing_a_null_memento
+		{
+			It should_return_null = () =>
+				new ConventionRoutingTable().Create(null).ShouldBeNull();
+		}
+		[Ignore("Still testing...")]
+		public class when_providing_a_memento
+		{
+			Establish context = () =>
+			{
+				memento = new TestMemento();
+				table = new ConventionRoutingTable(typeof(TestHydratable));
+			};
+
+			Because of = () =>
+				hydratable = table.Create(memento);
+
+			It should_create_a_hydratable = () =>
+				hydratable.ShouldBeOfType<TestHydratable>();
+
+			static IRoutingTable table;
+			static TestMemento memento;
+			static IHydratable hydratable;
+		}
+
+		private class TestMemento
+		{
+		}
+		private class TestHydratable : IHydratable
+		{
+			public static TestHydratable Create(TestMemento memento)
+			{
+				return new TestHydratable();
+			}
+
+			public string Key { get; private set; }
+			public bool IsComplete { get; private set; }
+			public bool IsPublicSnapshot { get; private set; }
+			public IEnumerable<object> GatherMessages()
+			{
+				throw new System.NotImplementedException();
+			}
+			public object GetMemento()
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+	}
+}
+// ReSharper restore InconsistentNaming
+#pragma warning restore 169
---
+++ src/Hydrospanner/Wireup/ConventionRoutingTable.cs
@@ -0,0 +1,82 @@
+﻿namespace Hydrospanner.Wireup
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Reflection;
+
+	public class ConventionRoutingTable : IRoutingTable
+    {
+        public IEnumerable<string> Lookup(object message, Dictionary<string, string> headers)
+        {
+            return null;
+        }
+        public IHydratable Create(object message, Dictionary<string, string> headers)
+        {
+            return null;
+        }
+        public IHydratable Create(object memento)
+        {
+            return null;
+        }
+
+		public ConventionRoutingTable()
+		{
+		}
+        public ConventionRoutingTable(params Assembly[] assemblies) : this(assemblies.SelectMany(x => x.GetTypes()))
+        {
+        }
+		public ConventionRoutingTable(params Type[] types) : this((IEnumerable<Type>)types)
+		{
+		}
+		public ConventionRoutingTable(IEnumerable<Type> types)
+		{
+
+			// TODO: Find methods with the following signatures:
+			// 1. "static Key(T, Dictionary<string, string>):string"
+			// 2. "static Create(T, Dictionary<string, string>):IHydratable"
+			// 3. "static Create(T):IHydratable"
+
+			// take each of the methods found and register it with the corresponding dictionary
+			// then during the lookup/create phases, invoke those various methods
+
+			foreach (var type in types ?? new Type[0])
+				this.RegisterMementoFactory(type);
+		}
+		private void RegisterMementoFactory(Type type)
+		{
+			var methods = type.GetMethods(BindingFlags.Static | BindingFlags.Public);
+			foreach (var method in methods)
+				RegisterMementoFactory(method);
+		}
+		private void RegisterMementoFactory(MethodInfo method)
+		{
+			if (method.IsGenericMethod)
+				return;
+
+			if (method.Name != FactoryMethodName)
+				return;
+
+			if (!typeof(IHydratable).IsAssignableFrom(method.ReturnType))
+				return;
+
+			var parameters = method.GetParameters(); // TODO: fork at this point and register the factory from a memento or message
+			if (parameters.Length != 1)
+				return;
+
+			var mementoType = parameters[0].ParameterType;
+			if (mementoType == typeof(object))
+				return;
+
+			this.mementos[mementoType] = MakeFactoryMethod(method, mementoType);
+		}
+
+		private static Func<object, IHydratable> MakeFactoryMethod(MethodInfo method, Type generic)
+		{
+			return null;
+		}
+
+		private const string FactoryMethodName = "Create";
+		private readonly Dictionary<Type, Func<object, IHydratable>> mementos = new Dictionary<Type, Func<object, IHydratable>>();
+    }
+}
