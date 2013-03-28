4fcb79e54c257a157a04bcc5b8de9d0756731fe1
Oliver, Jonathan <joliver@localhost>
2013-03-27 20:03:13 -0600

    Added some thoughts to the implementation.

--- src/Hydrospanner/Phases/Transformation/Transformer.cs
+++ src/Hydrospanner/Phases/Transformation/Transformer.cs
@@ -1,7 +1,6 @@
 ﻿namespace Hydrospanner.Phases.Transformation
 {
 	using System.Collections.Generic;
-	using System.Linq;
 	using Snapshot;
 
 	public interface ITransformer
@@ -9,34 +8,45 @@
 		IEnumerable<object> Handle(object message, Dictionary<string, string> headers, bool live);
 	}
 
-	public class Transformer : ITransformer
+	public sealed class Transformer : ITransformer
 	{
 		// TODO: delete and TDD; this is just spike code to see if I've got the concepts down...
 
 		public IEnumerable<object> Handle(object message, Dictionary<string, string> headers, bool live)
 		{
+			this.gathered.Clear();
+
+			// TODO: perhaps we should make the handle method receive the current sequence again?
+			// then we can compare it to the journaled sequence to see if we are live;
+			// I also noticed that the snapshot tracker needs the current sequence and is tracking
+			// that internally; would it be better to pass it in there as well?
 			this.currentSequence++;
 
-			var hydratables = this.repository.Load(message, headers);
+			var hydratables = this.repository.Load(message, headers); // this is responsible for creating if it doesn't exist (and tracking tombstones)
 
-			foreach (var h in hydratables.Where(h => !h.IsComplete))
+			// the repo will never hand back a completed hydratable
+			foreach (var h in hydratables)
 			{
 				h.Hydrate(message, headers, live);
 
-				if (h.IsPublicSnapshot || h.IsComplete)
-				{
-					var next = this.snapshotRing.Next();
-					var claimed = this.snapshotRing[next];
-					claimed.AsPublicSnapshot(h.Key, h.GetMemento(), this.currentSequence);
-					this.snapshotRing.Publish(next);
-					
-					if (h.IsComplete) // Not sure about this...
-						this.repository.Delete(h);
-				}
+				if (live)
+					this.gathered.AddRange(h.GatherMessages());
 
-				foreach (var m in h.GatherMessages())
-					yield return m;
+				if (h.IsPublicSnapshot || h.IsComplete)
+					this.TakeSnapshot(h);
+
+				if (h.IsComplete)
+					this.repository.Delete(h);
 			}
+
+			return this.gathered;
+		}
+		private void TakeSnapshot(IHydratable hydratable)
+		{
+			var next = this.snapshotRing.Next();
+			var claimed = this.snapshotRing[next];
+			claimed.AsPublicSnapshot(hydratable.Key, hydratable.GetMemento(), this.currentSequence);
+			this.snapshotRing.Publish(next);
 		}
 
 		public Transformer(IRepository repository, IRingBuffer<SnapshotItem> snapshotRing, long journaledSequence)
@@ -46,8 +56,9 @@
 			this.repository = repository;
 		}
 
-		readonly IRepository repository;
-		readonly IRingBuffer<SnapshotItem> snapshotRing;
-		long currentSequence;
+		private readonly List<object> gathered = new List<object>();
+		private readonly IRingBuffer<SnapshotItem> snapshotRing;
+		private readonly IRepository repository;
+		private long currentSequence;
 	}
 }
