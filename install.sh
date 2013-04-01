b3171a5f01188d5fe0853f15ea2fd92bad1a2849
Oliver, Jonathan <jonathan@localhost>
2013-04-01 14:57:44 -0600

    Added package restore for mono.

---
+++ install.sh
@@ -0,0 +1,7 @@
+mono ./src/.nuget/NuGet.exe install -SolutionDirectory ./src RabbitMQ.Client
+mono ./src/.nuget/NuGet.exe install -SolutionDirectory ./src disruptor-fork
+mono ./src/.nuget/NuGet.exe install -SolutionDirectory ./src Newtonsoft.Json
+mono ./src/.nuget/NuGet.exe install -SolutionDirectory ./src System.IO.Abstractions
+mono ./src/.nuget/NuGet.exe install -SolutionDirectory ./src MySql.Data
+mono ./src/.nuget/NuGet.exe install -SolutionDirectory ./src NSubstitute
+mono ./src/.nuget/NuGet.exe install -SolutionDirectory ./src Machine.Specifications-signed
