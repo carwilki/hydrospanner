60e0f07aa283fcdf13fa5b5e1e541b01cec1f7ad
Oliver, Jonathan <jonathan@localhost>
2013-04-11 13:55:19 -0600

    Simplified aggregate.

--- src/SampleApplication/FizzBuzzAggregate.cs
+++ src/SampleApplication/FizzBuzzAggregate.cs
@@ -19,43 +19,44 @@
 		public bool IsComplete { get { return this.Value == 15; } }
 		public List<object> Messages { get; private set; }
 
+		private void Append(object message, bool append)
+		{
+			if (append)
+				this.Messages.Add(message);
+		}
+
 		public void Increment(int value)
 		{
 			if (value % 3 == 0 && value % 5 == 0)
-				this.Apply(new FizzBuzzEvent { StreamId = this.streamId, Value = value }.Append(this.Messages));
+				this.Apply(new FizzBuzzEvent { StreamId = this.streamId, Value = value }, true);
 			else if (value % 5 == 0)
-				this.Apply(new BuzzEvent { StreamId = this.streamId, Value = value }.Append(this.Messages));
+				this.Apply(new BuzzEvent { StreamId = this.streamId, Value = value }, true);
 			else if (value % 3 == 0)
-				this.Apply(new FizzEvent { StreamId = this.streamId, Value = value }.Append(this.Messages));
+				this.Apply(new FizzEvent { StreamId = this.streamId, Value = value }, true);
 			else
-				this.Apply(new CountEvent { StreamId = this.streamId, Value = value }.Append(this.Messages));
+				this.Apply(new CountEvent { StreamId = this.streamId, Value = value }, true);
 		}
-		public void Apply(FizzBuzzEvent message)
+		public void Apply(FizzBuzzEvent message, bool append = false)
 		{
+			this.Append(message, append);
 			this.Value = message.Value;
 		}
-		public void Apply(BuzzEvent message)
+		public void Apply(BuzzEvent message, bool append = false)
 		{
+			this.Append(message, append);
 			this.Value = message.Value;
 		}
-		public void Apply(FizzEvent message)
+		public void Apply(FizzEvent message, bool append = false)
 		{
+			this.Append(message, append);
 			this.Value = message.Value;
 		}
-		public void Apply(CountEvent message)
+		public void Apply(CountEvent message, bool append = false)
 		{
+			this.Append(message, append);
 			this.Value = message.Value;
 		}
 
 		private readonly Guid streamId;
-	}
-
-	public static class AggregateExtensions
-	{
-		public static T Append<T>(this T message, List<object> messages)
-		{
-			messages.Add(message);
-			return message;
-		}
 	}
 }
