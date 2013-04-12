6b49ba1b508de7a0e5c4f9c3a74f7ca5ae1bf3e6
Oliver, Jonathan <jonathan@localhost>
2013-04-12 11:08:46 -0600

    Created nuget packages.

--- build.cmd
+++ build.cmd
@@ -7,19 +7,20 @@ msbuild src/Hydrospanner.sln /nologo /v:q /p:Configuration=Release /t:Clean
 msbuild src/Hydrospanner.sln /nologo /v:q /p:Configuration=Release /clp:ErrorsOnly
 
 echo Merging assemblies...
-if exist "src\merged" rmdir /s /q "src\merged"
-mkdir src\merged
-bin\ilmerge\ILMerge.exe /keyfile:src\Hydrospanner.snk /internalize /wildcards /target:library ^
+if exist "publish" rmdir /s /q "publish"
+mkdir publish\bin
+mkdir publish\pkg
+bin\ILMerge.exe /keyfile:src\Hydrospanner.snk /internalize /wildcards /target:library ^
  /targetplatform:"v4,C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319" ^
- /out:"src\merged\Hydrospanner.dll" ^
+ /out:"publish\bin\Hydrospanner.dll" ^
  "src/Hydrospanner/bin/Release/Hydrospanner.dll" ^
- "src/Hydrospanner/bin/Release/Atomic.dll.dll" ^
+ "src/Hydrospanner/bin/Release/Atomic.dll" ^
  "src/Hydrospanner/bin/Release/Disruptor.dll" ^
  "src/Hydrospanner/bin/Release/Newtonsoft.Json.dll" ^
  "src/Hydrospanner/bin/Release/RabbitMQ.Client.dll" ^
  "src/Hydrospanner/bin/Release/System.IO.Abstractions.dll"
  
 echo Creating NuGet packages...
-for /r %%i in (src\packages\Hydrospanner*.nuspec) do src\.nuget\nuget.exe pack %%i -symbols
+for /r %%i in (src\packages\Hydrospanner*.nuspec) do src\.nuget\nuget.exe pack %%i -symbols -OutputDirectory publish\pkg
 
 echo Done.
