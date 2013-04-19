d57370df41fa56868bcaa1c2ba0ed66aa17ec562
Oliver, Jonathan <jonathan@localhost>
2013-04-19 14:28:05 -0600

    Split timeout hydratable and separated out aggregate which contains behavior.

---
+++ src/Hydrospanner/Timeout/TimeoutAggregate.cs
@@ -0,0 +1,78 @@
+﻿namespace Hydrospanner.Timeout
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class TimeoutAggregate
+	{
+		public List<object> Messages { get; private set; } 
+
+		public void Handle(CurrentTimeMessage message)
+		{
+			foreach (var item in this.timeouts)
+			{
+				if (item.Key > message.UtcNow)
+					break;
+
+				foreach (var value in item.Value)
+					this.Append(this.Apply, new TimeoutElapsedEvent(value.Key, item.Key, value.Value));
+			}
+		}
+		private void Append<T>(Action<T> callback, T message)
+		{
+			this.Messages.Add(message);
+			callback(message);
+		}
+		public void Apply(TimeoutRequestedEvent message)
+		{
+			List<KeyValuePair<string, int>> items;
+			if (!this.timeouts.TryGetValue(message.Timeout, out items))
+				this.timeouts[message.Timeout] = items = new List<KeyValuePair<string, int>>();
+
+			this.count++;
+			items.Add(new KeyValuePair<string, int>(message.Key, message.State));
+		}
+		public void Apply(TimeoutElapsedEvent message)
+		{
+			List<KeyValuePair<string, int>> items;
+			if (!this.timeouts.TryGetValue(message.Timeout, out items))
+				return;
+
+			this.count = this.count - items.RemoveAll(x => x.Key == message.Key && x.Value == message.State);
+			if (items.Count == 0)
+				this.timeouts.Remove(message.Timeout);
+		}
+
+		public object GetMemento()
+		{
+			var list = new List<TimeoutEntry>(this.count);
+			foreach (var item in this.timeouts)
+				foreach (var value in item.Value)
+					list.Add(new TimeoutEntry(value.Key, item.Key, value.Value));
+
+			return new TimeoutMemento { Timeouts = list };
+		}
+
+		public TimeoutAggregate()
+		{
+			this.Messages = new List<object>();
+		}
+		public TimeoutAggregate(TimeoutMemento memento) : this()
+		{
+			this.timeouts = new SortedList<DateTime, List<KeyValuePair<string, int>>>(memento.Timeouts.Count);
+
+			foreach (var item in memento.Timeouts)
+			{
+				List<KeyValuePair<string, int>> list;
+				if (!this.timeouts.TryGetValue(item.Timeout, out list))
+					this.timeouts[item.Timeout] = list = new List<KeyValuePair<string, int>>();
+
+				this.count++;
+				list.Add(new KeyValuePair<string, int>(item.Key, item.State));
+			}
+		}
+
+		private readonly SortedList<DateTime, List<KeyValuePair<string, int>>> timeouts;
+		private int count;
+	}
+}
--- src/Hydrospanner/Timeout/TimeoutHydratable.cs
+++ src/Hydrospanner/Timeout/TimeoutHydratable.cs
@@ -1,12 +1,11 @@
 ﻿namespace Hydrospanner.Timeout
 {
-	using System;
 	using System.Collections.Generic;
 
 	public sealed class TimeoutHydratable : IHydratable,
+		IHydratable<CurrentTimeMessage>,
 		IHydratable<TimeoutRequestedEvent>,
-		IHydratable<TimeoutElapsedEvent>,
-		IHydratable<CurrentTimeMessage>
+		IHydratable<TimeoutElapsedEvent>
 	{
 		public string Key
 		{
@@ -21,65 +20,42 @@
 			get { return false; }
 		}
 
+		public void Hydrate(CurrentTimeMessage message, Dictionary<string, string> headers, bool live)
+		{
+			if (live)
+				this.aggregate.Handle(message);
+		}
+		public void Hydrate(TimeoutRequestedEvent message, Dictionary<string, string> headers, bool live)
+		{
+			if (!live)
+				this.aggregate.Apply(message);
+		}
+		public void Hydrate(TimeoutElapsedEvent message, Dictionary<string, string> headers, bool live)
+		{
+			if (live)
+				this.aggregate.Apply(message);
+		}
 		public IEnumerable<object> GatherMessages()
 		{
-			if (this.gathered.Count == 0)
-				yield break;
+			var messages = this.aggregate.Messages;
+			foreach (var message in messages)
+				yield return message;
 
-			foreach (var item in this.gathered)
-				yield return item;
-
-			this.gathered.Clear();
+			messages.Clear();
 		}
 
 		public object GetMemento()
 		{
-			var list = new List<TimeoutEntry>(this.count);
-			foreach (var item in this.timeouts)
-				foreach (var value in item.Value)
-					list.Add(new TimeoutEntry(value.Key, item.Key, value.Value));
-
-			return new TimeoutMemento { Timeouts = list };
+			return this.aggregate.GetMemento();
 		}
 		public static TimeoutHydratable Restore(TimeoutMemento memento)
 		{
-			return new TimeoutHydratable(memento.Timeouts);
+			return new TimeoutHydratable(memento);
 		}
 
-		public void Hydrate(TimeoutRequestedEvent message, Dictionary<string, string> headers, bool live)
+		public static HydrationInfo Lookup(CurrentTimeMessage message, Dictionary<string, string> headers)
 		{
-			if (live && message.Timeout <= DateTime.UtcNow)
-				this.gathered.Add(message);
-			else
-			{
-				List<KeyValuePair<string, int>> items;
-				if (!this.timeouts.TryGetValue(message.Timeout, out items))
-					this.timeouts[message.Timeout] = items = new List<KeyValuePair<string, int>>();
-
-				this.count++;
-				items.Add(new KeyValuePair<string, int>(message.Key, message.State));
-			}
-		}
-		public void Hydrate(TimeoutElapsedEvent message, Dictionary<string, string> headers, bool live)
-		{
-			List<KeyValuePair<string, int>> items;
-			if (!this.timeouts.TryGetValue(message.Timeout, out items))
-				return;
-
-			this.count = this.count - items.RemoveAll(x => x.Key == message.Key && x.Value == message.State);
-			if (items.Count == 0)
-				this.timeouts.Remove(message.Timeout);
-		}
-		public void Hydrate(CurrentTimeMessage message, Dictionary<string, string> headers, bool live)
-		{
-			foreach (var item in this.timeouts)
-			{
-				if (item.Key > message.UtcNow)
-					break;
-
-				foreach (var value in item.Value)
-					this.gathered.Add(new TimeoutElapsedEvent(value.Key, item.Key, value.Value));
-			}
+			return new HydrationInfo(HydratableKey, () => new TimeoutHydratable());
 		}
 		public static HydrationInfo Lookup(TimeoutRequestedEvent message, Dictionary<string, string> headers)
 		{
@@ -89,33 +65,17 @@
 		{
 			return new HydrationInfo(HydratableKey, () => new TimeoutHydratable());
 		}
-		public static HydrationInfo Lookup(CurrentTimeMessage message, Dictionary<string, string> headers)
+
+		public TimeoutHydratable()
 		{
-			return new HydrationInfo(HydratableKey, () => new TimeoutHydratable());
+			this.aggregate = new TimeoutAggregate();
 		}
-
-		public TimeoutHydratable() : this(new List<TimeoutEntry>())
+		public TimeoutHydratable(TimeoutMemento memento)
 		{
-		}
-		public TimeoutHydratable(ICollection<TimeoutEntry> timeouts)
-		{
-			timeouts = timeouts ?? new TimeoutEntry[0];
-			this.timeouts = new SortedList<DateTime, List<KeyValuePair<string, int>>>(timeouts.Count);
-
-			foreach (var item in timeouts)
-			{
-				List<KeyValuePair<string, int>> items;
-				if (!this.timeouts.TryGetValue(item.Timeout, out items))
-					this.timeouts[item.Timeout] = items = new List<KeyValuePair<string, int>>();
-
-				this.count++;
-				items.Add(new KeyValuePair<string, int>(item.Key, item.State));
-			}
+			this.aggregate = new TimeoutAggregate(memento);
 		}
 
 		private const string HydratableKey = "/internal/timeout";
-		private readonly List<object> gathered = new List<object>();
-		private readonly SortedList<DateTime, List<KeyValuePair<string, int>>> timeouts;
-		private int count;
+		private readonly TimeoutAggregate aggregate;
 	}
 }
