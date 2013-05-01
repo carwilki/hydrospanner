6e04e53160fa9feb1640ea05b5034fa725238899
Oliver, Jonathan <jonathan@localhost>
2013-04-30 19:59:43 -0600

    Corrected integration test so that duplicate identifiers are loaded from most recent backward.

--- src/Hydrospanner.IntegrationTests/SqlBootstrapStoreTests.cs
+++ src/Hydrospanner.IntegrationTests/SqlBootstrapStoreTests.cs
@@ -54,11 +54,11 @@ namespace Hydrospanner.IntegrationTests
 				var types = new[] { "asdf", "qwer" };
 				var foreignIds = new[]
 				{
-					Guid.Parse("11111111-1111-1111-1111-111111111111"),
-					Guid.Parse("22222222-2222-2222-2222-222222222222"),
-					Guid.Parse("33333333-3333-3333-3333-333333333333"),
+					Guid.Parse("66666666-6666-6666-6666-666666666666"),
+					Guid.Parse("55555555-5555-5555-5555-555555555555"),
 					Guid.Parse("44444444-4444-4444-4444-444444444444"),
-					Guid.Parse("55555555-5555-5555-5555-555555555555")
+					Guid.Parse("33333333-3333-3333-3333-333333333333"),
+					Guid.Parse("22222222-2222-2222-2222-222222222222")
 				};
 				result.ShouldBeLike(new BootstrapInfo(6, 5, types, foreignIds));
 			};
