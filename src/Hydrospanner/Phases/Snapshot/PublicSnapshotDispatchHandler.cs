a7539b4edd2c0ad9a6d8ab0a5e2d6f01e46272ab
Oliver, Jonathan <jonathan@localhost>
2013-05-06 10:08:49 -0600

    Added messaging handler (not yet implemented or referenced) to facilitate dispatch of public snapshots (projections).

---
+++ src/Hydrospanner/Phases/Snapshot/PublicSnapshotDispatchHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Hydrospanner.Phases.Snapshot
+{
+	using Disruptor;
+
+	public class PublicSnapshotDispatchHandler : IEventHandler<SnapshotItem>
+	{
+		public void OnNext(SnapshotItem data, long sequence, bool endOfBatch)
+		{
+			// FUTURE: this will transactionally publish snapshots to the wire in a standard format
+		}
+	}
+}
